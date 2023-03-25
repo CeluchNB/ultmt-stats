5171c1f5c965a195838cb2e636284f93455f4bd0
Noah Celuch <noah.celuch@gmail.com>
2023-03-25 15:33:44 -0400

    feat: update express parser

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/ultmt-stats', (req, res) => {
     return res.json({ message: 'The statistics microservice of The Ultmt App' })
 })
 
 app.post('/game/ingest', (req, res) => {
-    console.log('got request', JSON.stringify(req.body))
+    console.log('got request', req.body)
     return res.status(201).send()
 })
 
