60a5ac8b300944224c13486219961cc4a602bf0a
Noah Celuch <noah.celuch@gmail.com>
2023-03-25 15:03:40 -0400

    feat: add test endpoint to ingest game

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,9 @@ app.get('/ultmt-stats', (req, res) => {
     return res.json({ message: 'The statistics microservice of The Ultmt App' })
 })
 
+app.post('/game/ingest', (req, res) => {
+    console.log('got request', JSON.stringify(req.body))
+    return res.status(201).send()
+})
+
 export default app
