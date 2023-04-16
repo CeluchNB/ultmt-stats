61417b565bc590364805bd6ab4a90efbd9f1b6cc
Noah Celuch <noah.celuch@gmail.com>
2023-04-15 23:22:28 -0400

    fix: added console debugging statements

--- src/routes/v1/game.ts
+++ src/routes/v1/game.ts
@@ -7,9 +7,11 @@ export const gameRouter = Router()
 
 gameRouter.post('/game', body('game').isObject(), async (req: Request, res: Response, next) => {
     try {
+        console.log('got game', req.body.game)
         await createGame(req.body.game)
         return res.status(201).send()
     } catch (error) {
+        console.log('got error', error)
         next(error)
     }
 })
