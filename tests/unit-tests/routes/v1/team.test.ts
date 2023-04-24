65803ad09f77d2d2ad00c223e6ed7638864a4b5b
Noah Celuch <noah.celuch@gmail.com>
2023-04-23 20:11:14 -0400

    feat(team): added filter team endpoint

--- tests/unit-tests/routes/v1/team.test.ts
+++ tests/unit-tests/routes/v1/team.test.ts
@@ -6,6 +6,7 @@ import { teamOne } from '../../../fixtures/data'
 import { getInitialTeamData } from '../../../../src/utils/team-stats'
 import app from '../../../../src/app'
 import { Types } from 'mongoose'
+import AtomicTeam from '../../../../src/models/atomic-team'
 
 beforeAll(async () => {
     await setUpDatabase()
@@ -33,6 +34,60 @@ describe('/GET team by id', () => {
 
     it('with unfound team', async () => {
         const response = await request(app).get(`/api/v1/stats/team/${new Types.ObjectId()}`).expect(404)
+        expect(response.body.message).toBe(Constants.TEAM_NOT_FOUND)
+    })
+})
+
+describe('/GET filter team stats', () => {
+    const gameOneId = new Types.ObjectId()
+    const gameTwoId = new Types.ObjectId()
+    const gameThreeId = new Types.ObjectId()
+
+    beforeEach(async () => {
+        await Team.create({
+            ...teamOne,
+            ...getInitialTeamData({ goalsFor: 11, goalsAgainst: 11, wins: 17, losses: 4 }),
+        })
+
+        await AtomicTeam.create({
+            teamId: teamOne._id,
+            gameId: gameOneId,
+            ...getInitialTeamData({ goalsFor: 1, goalsAgainst: 2, wins: 0, losses: 1 }),
+        })
+        await AtomicTeam.create({
+            teamId: teamOne._id,
+            gameId: gameTwoId,
+            ...getInitialTeamData({ goalsFor: 5, goalsAgainst: 4, wins: 1, losses: 0 }),
+        })
+
+        await AtomicTeam.create({
+            teamId: teamOne._id,
+            gameId: gameThreeId,
+            ...getInitialTeamData({ goalsFor: 5, goalsAgainst: 4, wins: 1, losses: 0 }),
+        })
+    })
+
+    it('with team', async () => {
+        const response = await request(app)
+            .get(`/api/v1/stats/filter/team/${teamOne._id}?games=${gameOneId},${gameTwoId}`)
+            .expect(200)
+
+        const { team } = response.body
+        expect(team).toMatchObject({
+            _id: teamOne._id.toHexString(),
+            name: teamOne.name,
+            goalsFor: 6,
+            goalsAgainst: 6,
+            wins: 1,
+            losses: 1,
+        })
+    })
+
+    it('with unfound team', async () => {
+        const response = await request(app)
+            .get(`/api/v1/stats/filter/team/${new Types.ObjectId()}?games=${gameOneId},${gameTwoId}`)
+            .expect(404)
+
         expect(response.body.message).toBe(Constants.TEAM_NOT_FOUND)
     })
 })
